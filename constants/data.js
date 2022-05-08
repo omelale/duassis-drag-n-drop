b08291e1f4c6e68c751fbc83bb77cc25551b97ea
Ajten <ajtencani@gmail.com>
2022-05-08 23:43:30 +0200

    initialized mockup data

---
+++ constants/data.js
@@ -0,0 +1,72 @@
+const teams = 2; 
+
+const playerPerTeam = 6;
+
+const players = [
+    {
+        id: 1,
+        name: 'Shaban',
+        lastName: 'R',
+    },
+    {
+        id: 2,
+        name: 'Andi',
+        lastName: 'X',
+    },
+    {
+        id: 3,
+        name: 'Ajten',
+        lastName: 'C',
+    },
+    {
+        id: 4,
+        name: 'Fabian',
+        lastName: 'E',
+    },
+    {
+        id: 5,
+        name: 'Erik',
+        lastName: 'K',
+    },
+    {
+        id: 6,
+        name: 'Bruno',
+        lastName: 'B',
+    },
+    {
+        id: 7,
+        name: 'Aris',
+        lastName: 'K',
+    },
+    {
+        id: 8,
+        name: 'Danjel',
+        lastName: 'B',
+    },
+    {
+        id: 8,
+        name: 'Ketjon',
+        lastName: 'F',
+    },
+    {
+        id: 9,
+        name: 'Aldo',
+        lastName: 'I',
+    },
+    {
+        id: 10,
+        name: 'Albi',
+        lastName: 'X',
+    },
+    {
+        id: 11,
+        name: 'Erion',
+        lastName: 'E',
+    },
+    {
+        id: 12,
+        name: 'Genti',
+        lastName: 'S',
+    },
+    
+];
