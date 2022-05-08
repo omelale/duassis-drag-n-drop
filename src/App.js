ddc3a7fb77657925ab1f13d8dac1fba68d81193c
Ajten <ajtencani@gmail.com>
2022-05-09 00:39:31 +0200

    initial setup of the drag and drop container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 import data from './constants/data';
 
 function App() {
-	return (
-		<div className="App">
-			<ul>
-				{data.players.map((player) => {
-					
-					return <li key={player.id}>{player.name}</li>
-				})}
-			</ul>
-			
-		</div>
-	);
+  const [players, updatePlayers] = useState(data.players);
+
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = Array.from(players);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updatePlayers(items);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Drag and drop the player into the team you wish</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="players">
+            {(provided) => (
+              <ul className="players" {...provided.droppableProps} ref={provided.innerRef}>
+                {players.map(({ id, name, lastName }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id.toString()} index={index}>
+                      {(provided) => (
+                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          <p>
+                            {name + ' ' + lastName}
+                          </p>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </header>
+    </div>
+  );
 }
 
 export default App;
