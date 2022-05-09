fb64bc7ba6004185ce59d23890e93c41ee2f33f1
Ajten <ajtencani@gmail.com>
2022-05-09 23:40:48 +0200

    visual modifications

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,17 +5,19 @@ import Player from './Player'
 
 function Team(props) {
     return (
-        <Droppable droppableId={props.id.toString()} className="container">
-            {(provided, snapshot) => (
-                <div id="players" {...provided.droppableProps} ref={provided.innerRef} className={snapshot.isDraggingOver ? 'isDraggingOver' : ''} >
-                    <h3>{props.name}</h3>
-                    {props.players.map((player, index) => {
-                        return <Player key={player.id} player={player} id={player.id} index={index} />
-                    })}
-                    {provided.placeholder}
-                </div>
-            )}
-        </Droppable>
+        <div className="container" id="players">
+            <h3>{props.name}</h3>
+            <Droppable droppableId={props.id.toString()} className="container">
+                {(provided, snapshot) => (
+                    <div id="player-container" {...provided.droppableProps} ref={provided.innerRef} className={snapshot.isDraggingOver ? 'isDraggingOver' : ''} >
+                        {props.players.map((player, index) => {
+                            return <Player key={player.id} player={player} id={player.id} index={index} />
+                        })}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </div>
     )
 }
 
