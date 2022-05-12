88be9805427afcd1c3b97f822b9b91721560627b
Ajten <ajtencani@gmail.com>
2022-05-12 20:01:00 +0200

    display formations on large screen

--- src/components/PlayerBall.js
+++ src/components/PlayerBall.js
@@ -1,10 +1,50 @@
 import React from 'react'
 
+const calcPosition = (team, role) => {
+    if (team % 2 === 0) {
+        // right team
+        switch (role) {
+            case 'gk':
+                return { position: 'absolute', top: '50%', right: '8%' }
+            case 'rd':
+                return { position: 'absolute', top: '30%', right: '25%' }
+            case 'ld':
+                return { position: 'absolute', top: '60%', right: '25%' }
+            case 'cm':
+                return { position: 'absolute', top: '50%', right: '50%' }
+            case 'ra':
+                return { position: 'absolute', top: '23%', right: '80%' }
+            case 'la':
+                return { position: 'absolute', top: '69%', right: '80%' }
+            default:
+                return { position: 'absolute', top: '50%', right: '8000%' }
+        }
+    }
+    switch (role) {
+        case 'gk':
+            return { position: 'absolute', top: '50%', left: '8%' }
+        case 'rd':
+            return { position: 'absolute', top: '70%', left: '26%' }
+        case 'ld':
+            return { position: 'absolute', top: '25%', left: '26%' }
+        case 'cm':
+            return { position: 'absolute', top: '50%', left: '50%' }
+        case 'ra':
+            return { position: 'absolute', top: '70%', left: '75%' }
+        case 'la':
+            return { position: 'absolute', top: '25%', left: '75%' }
+        default:
+            return { position: 'absolute', top: '50%', left: '2000%' }
+    }
+}
 function PlayerBall(props) {
-  console.log(props);
-  return (
-    <div className="player-ball">{props.player.name}</div>
-  )
+    const bgcolor = props.team % 2 === 0 ? 'white' : 'black'
+    return (
+        <div className="player-position" style={calcPosition(props.team, props.role.id)}>
+            <div className="player-ball" style={{ backgroundColor: bgcolor }}></div>
+            <div className="name">{props.player.name}</div>
+        </div>
+    )
 }
 
 export default PlayerBall
--- src/components/Teamfield.js
+++ src/components/Teamfield.js
@@ -4,12 +4,18 @@ import PlayerBall from './PlayerBall'
 function Teamfield(props) {
     return (
         <div className="teamfield" >
-            <h2 className="team-name">{props.team.name}</h2>
-            {props.team.playerIds.map((playerId, index) => {
-                const player = props.players.find(p => p.id === playerId)
-                const role = props.roles[index];
-                return <PlayerBall player={player} role={role} key={player.id}/>
-            })}
+            <div className="team-info">
+                <h2 className="team-name">
+                    {props.team.name}
+                </h2>
+            </div>
+            <div className="team-line-up">
+                {props.team.playerIds.map((playerId, index) => {
+                    const player = props.players.find(p => p.id === playerId)
+                    const role = index + 1 <= props.players.length/2 ? props.roles[index] : 999;
+                    return <PlayerBall team={props.team.id} player={player} role={role} key={player.id} />
+                })}
+            </div>
         </div>
     )
 }
